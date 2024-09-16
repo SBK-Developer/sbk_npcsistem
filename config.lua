e1d65eb70c34b23608d06bdbdbfe6564bc53e30b
SBK-Developer <darkpipefivem@gmail.com>
2024-09-16 10:27:41 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.displayText = true -- Whether you want the heading text to show or not
+Config.displayDistance = 20.0 -- From how far to start displaying text
+Config.displayColor = "~g~"
+
+Config.Peds = {
+    -- { x, y, z, ped heading, model hash, ped model, heading text, animation info }
+    {2408.7356, 3030.2786, 48.1526, 351.4323, 0xC99F21C4,"a_m_y_business_01", "Business Man", "mini@strip_club@idles@bouncer@base"},
+    {660.8044, 1282.4446, 360.2958, 275.0204, 0xC99F21C4,"a_m_y_business_01", "Business Man", "mini@strip_club@idles@bouncer@base"}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'SBK'
+description 'SBK PedGenerator Sistem Script'
+version '1.0.'
+
+
+-- What to run
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+export 'addNPC'
